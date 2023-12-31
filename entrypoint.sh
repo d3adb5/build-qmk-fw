548c8d77d57b20fd801e6f6fb7ca1c22becd8dc5
d3adb5 <me@d3adb5.net>
2023-12-31 01:29:58 -0800

    refactor: guarantee qmk_firmware will be under /opt
    Guarantee the path to the qmk_firmware clone will be known by placing it
    under /opt.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ readonly keyboard="$1"
 readonly keymap="$2"
 readonly qmk_output="$3"
 
+qmk config user.qmk_home=/opt/qmk_firmware
 qmk compile -kb "$keyboard" -km "$keymap"
 
 mkdir "$qmk_output"
-find "/qmk_firmware/.build" -name '*.hex' -or -name '*.bin' -exec cp -v {} "$qmk_output" \;
+find "/opt/qmk_firmware/.build" -name '*.hex' -or -name '*.bin' -exec cp -v {} "$qmk_output" \;
