c5894f0e6eed1658e4bb2d163c18690b5918232e
d3adb5 <me@d3adb5.net>
2023-12-31 02:04:37 -0800

    feat: allow copying local keymap into qmk home
    Allow copying a local keymap directory into the target keyboard's
    keymaps directory.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,24 @@
 readonly keyboard="$1"
 readonly keymap="$2"
 readonly qmk_output="$3"
+readonly local_keymap="$4"
+
+# Find the keymaps directory the same way QMK CLI does
+if [ -n "$local_keymap" ]; then
+  keymap_lookup_dir="/opt/qmk_firmware/keyboards/$keyboard"
+
+  until find "$keymap_lookup_dir" -type d -name keymaps | grep -q .; do
+    keymap_lookup_dir=$(dirname "$keymap_lookup_dir")
+  done
+
+  if [ "$keymap_lookup_dir" = "/opt/qmk_firmware/keyboards" ]; then
+    echo "Could not find keymaps directory for $keyboard"
+    exit 1
+  fi
+
+  echo "Copying local keymap into $keymap_lookup_dir/keymaps"
+  cp -rv "$local_keymap" "$keymap_lookup_dir/keymaps/$(basename "$local_keymap")"
+fi
 
 qmk config user.qmk_home=/opt/qmk_firmware
 qmk compile -kb "$keyboard" -km "$keymap"
