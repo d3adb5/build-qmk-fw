62d1f7d673864cbe6b9574696353ed60a0d1a6d4
d3adb5 <me@d3adb5.net>
2023-12-31 01:17:43 -0800

    fix: grab build artifacts from /qmk_firmware
    Grab build artifacts from /qmk_firmware instead of $HOME/qmk_firmware.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ readonly keymap="$2"
 qmk compile -kb "$keyboard" -km "$keymap"
 
 mkdir output
-find "$HOME/qmk_firmware" -name '*.hex' -or -name '*.bin' -exec cp {} ./output \;
+find "/qmk_firmware/.build" -name '*.hex' -or -name '*.bin' -exec cp {} ./output \;
 ls output
