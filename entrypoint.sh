00bfad465d68412d568143a9ba7c0a9a355a1aa9
medoix <medoix@shivv.com>
2024-04-29 14:33:49 +1000

    Remove debug line and let QMK CLI check conversion option

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,14 +8,6 @@ readonly controller="$3"
 readonly qmk_output="$4"
 readonly local_keymap="$5"
 
-# Check if the controller matches one of the available options
-if [ "$controller" ]; then
-  if [ "$controller" != "proton_c" ] && [ "$controller" != "kb2040" ] && [ "$controller" != "promicro_rp2040" ] && [ "$controller" != "blok" ] && [ "$controller" != "bit_c_pro" ] && [ "$controller" != "stemcell" ] && [ "$controller" != "bonsai_c4" ] && [ "$controller" != "rp2040_ce" ] && [ "$controller" != "elite_pi" ] && [ "$controller" != "helios" ] && [ "$controller" != "liatris" ] && [ "$controller" != "imera" ] && [ "$controller" != "michi" ]; then
-    echo "Invalid convert option $keyboard"
-    exit 1
-  fi
-fi
-
 # Find the keymaps directory the same way QMK CLI does
 if [ -n "$local_keymap" ]; then
   keymap_lookup_dir="/opt/qmk_firmware/keyboards/$keyboard"
@@ -34,7 +26,6 @@ if [ -n "$local_keymap" ]; then
 fi
 
 qmk config user.qmk_home=/opt/qmk_firmware
-echo Controller - "$controller"
 qmk compile -kb "$keyboard" -km "$keymap" ${controller:+-e CONVERT_TO="$controller"}
 
 mkdir "$qmk_output"
