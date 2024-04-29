81026dc271cf9aa1c36725502b9e95bb495ef6bd
medoix <medoix@shivv.com>
2024-04-29 12:20:35 +1000

    Add controller conversion support

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,17 @@ set -e
 
 readonly keyboard="$1"
 readonly keymap="$2"
-readonly qmk_output="$3"
-readonly local_keymap="$4"
+readonly controller="$3"
+readonly qmk_output="$4"
+readonly local_keymap="$5"
+
+# Check if the controller matches one of the available options
+if [ "$controller" ]; then
+  if [ "$controller" != "proton_c" ] && [ "$controller" != "kb2040" ] && [ "$controller" != "promicro_rp2040" ] && [ "$controller" != "blok" ] && [ "$controller" != "bit_c_pro" ] && [ "$controller" != "stemcell" ] && [ "$controller" != "bonsai_c4" ] && [ "$controller" != "rp2040_ce" ] && [ "$controller" != "elite_pi" ] && [ "$controller" != "helios" ] && [ "$controller" != "liatris" ] && [ "$controller" != "imera" ] && [ "$controller" != "michi" ]; then
+    echo "Invalid convert option $keyboard"
+    exit 1
+  fi
+fi
 
 # Find the keymaps directory the same way QMK CLI does
 if [ -n "$local_keymap" ]; then
@@ -25,10 +34,11 @@ if [ -n "$local_keymap" ]; then
 fi
 
 qmk config user.qmk_home=/opt/qmk_firmware
-qmk compile -kb "$keyboard" -km "$keymap"
+echo Controller - "$controller"
+qmk compile -kb "$keyboard" -km "$keymap" ${controller:+-e CONVERT_TO="$controller"}
 
 mkdir "$qmk_output"
-find "/opt/qmk_firmware/.build" \( -name '*.hex' -or -name '*.bin' \) -exec cp -v {} "$qmk_output" \;
+find "/opt/qmk_firmware/.build" \( -name '*.hex' -or -name '*.bin' -or -name '*.uf2' \) -exec cp -v {} "$qmk_output" \;
 
 echo "built-images=$(find "$qmk_output" -type f | sed "s|^$qmk_output||" | paste -sd ',')" \
   >> "$GITHUB_OUTPUT"
